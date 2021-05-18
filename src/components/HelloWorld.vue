cd881d0715b0c01ab495420ed69d88af8743a573
pedja1990 <predragmiljus11@gmail.com>
2021-05-18 19:01:10 +0200

    zadatak 1,2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div >
+
+    <h1>Animals</h1>
+
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Species</th>
+          <th>Name</th>
+          <th>Date of birth</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(animal, key) in animals" :key="key">
+          <td>{{ animal.species }}</td>
+          <td>{{ animal.name }}</td>
+          <td>{{ animal.dateOfBirth ? animal.dateOfBirth : 'Nepoznat' }}</td>
+        </tr>
+      </tbody>
+    </table>
+    
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  
+ 
+  data() {
+    return {
+      
+      animals: [
+        {
+          species: 'Dog',
+          name: '',
+          dateOfBirth: '17-05-2021',
+       
+        },
+        {
+          species: 'Cat',
+          name: 'tom',
+          dateOfBirth: '05-05-2021',
+          
+        },
+        {
+          species: 'Fish',
+          name: 'fish',
+          dateOfBirth: '',
+          
+        },
+         {
+          species: 'Tiger',
+          name: 'SherKhan',
+          dateOfBirth: '',
+          
+        },
+          {
+          species: 'Lion',
+          name: 'Simba',
+          dateOfBirth: '',
+          
+        },
+
+      ],
+    };
   }
+
+
+
+
+
+
+
 }
 </script>
 
